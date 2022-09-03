cb237614667d518c60816a48a27fa785f81b7004
Hvhudu <shtormbreker2@gmail.com>
2022-09-03 19:39:28 +0300

    Add project files.

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Runtime.InteropServices;
+
+
+
+namespace HomeWork2.Models
+{
+    internal class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int PositionID { get; set; }
+        public int ID { get; set; }
+
+        public EmployeeModel(string firstName, string lastName, string dateOfBirth, int positionID, int id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = DateTime.Parse(dateOfBirth.ToString());
+            PositionID = positionID;
+            ID = id;
+
+        }
+        public override string ToString()
+        {
+            return $"{FirstName}\n{LastName}\n{DateOfBirth}\n{PositionID}";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HomeWork2.Models;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            EmployeeModel employee2 = new EmployeeModel("Oleg", "Dmitriev", "2004.07.24", 1, 1007);
+            
+            int res = DataLayers.DL.Employee.Update(employee2);
+            
+            Console.WriteLine($"Изменена  {res} строка"); ;
+            
+        }
+    }
+}
